9b313330771af82eba91e79a514114db2f5177d3
mosimchah <mosimchah@gmail.com>
2021-01-14 01:53:41 -0500

    msm8996-common: remove libfui (was libgui) deps
    Change-Id: Ie4143c300287575a18d5da038aa6b4d13ec85b42
    Signed-off-by: Nanhumly <admin@warbler.cc>

--- msm8996.mk
+++ msm8996.mk
@@ -138,7 +138,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
     camera.msm8996 \
-    libfui \
     vendor.qti.hardware.camera.device@1.0
 
 # Common init scripts
